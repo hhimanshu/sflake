5a79138b5521522dc36b2fbf654883262b754ccf
hhimanshu <dev.atwork@yahoo.com>
2024-03-08 13:07:51 +1300

    Add create_database function and display_connection_details function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
-from sflake import get_connection
+from sflake import create_database, display_connection_details, get_connection
 from snowflake.connector import SnowflakeConnection
+from snowflake.core import Root
+from snowflake.core.database import Database
 
 # Load the .env file
 load_dotenv()
@@ -12,15 +14,10 @@ def main():
     user = os.getenv("SNOWFLAKE_USER")
     password = os.getenv("SNOWFLAKE_PASSWORD")
     conn = get_connection(user=user, password=password, account=endpoint)
+    create_database(conn=conn, name="DEMO_DB")
+
     display_connection_details(conn)
 
-def display_connection_details(conn: SnowflakeConnection):
-    print(f"---------- Connection details -----------")
-    print(f"Account: {conn.account}")
-    print(f"User: {conn.user}")
-    print(f"Role: {conn.role}")
-    print(f"Warehouse: {conn.warehouse}")
-    print("-"*40)
 
 if __name__ == "__main__":
     main()
